4683899bb56659a4636d066990d374f67acb2f6a
Bolu Ojuko <boluojuko@gmail.com>
2017-07-12 11:10:43 -0400

    second commit

---
+++ function.js
@@ -0,0 +1,30 @@
+//function welloHorld(){
+  //   console.log("Wello Horld!");
+  //  return "Wello Horld!";
+//}
+//welloHorld();
+
+//Math.random(8008)
+
+function rand(num){
+ //this function returns a number between 0 and num
+var randNum = Math.random()*num;
+var result = Math.floor(randNum);
+return result
+}
+
+rand(14); //any nuber can replace 14
+
+
+function rand(num){
+    return Math.floor(Math.random()*num);
+}
+rand(95);
+//this is an easier way to do the above function
+
+
+
+function rand(num){
+    return Math.floor(Math.random()*num);
+}
+console.log(rand(95));
